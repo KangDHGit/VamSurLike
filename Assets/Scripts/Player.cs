1ff64f7cb6bd7276bf02cfd7b503010a435fe4b6
KangDHGit <eogks0323@gmail.com>
2022-12-24 14:22:15 +0900

    캐릭터 위치이동 구현
    rigid.MovePosition 활용
    AddForce, velocity 방법도 가능

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Components
+    Rigidbody2D _rigid;
+    #endregion
+
+    [SerializeField] Vector2 _inputVec;
+    [SerializeField] float _moveSpeed; public float MoveSpeed { get { return _moveSpeed; } }
+
+    private void Awake()
     {
         
+    }
+
+    void Start()
+    {
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Input();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Init()
+    {
+        if(!TryGetComponent(out _rigid))
+            Debug.LogError("Player _rigid is Null");
+        _moveSpeed = 5.0f;
+    }
+
+    void Input()
+    {
+        float x = UnityEngine.Input.GetAxisRaw("Horizontal");
+        float y = UnityEngine.Input.GetAxisRaw("Vertical");
+        _inputVec = new Vector2(x, y).normalized;
+    }
+
+    void Move()
+    {
+        //_rigid.velocity = _inputVec * _moveSpeed;
+        _rigid.MovePosition(_rigid.position + (_inputVec * _moveSpeed *Time.fixedDeltaTime));
     }
 }
