6838947957b1cc64119d4f3d28daa01705e76fa2
KangDHGit <eogks0323@gmail.com>
2022-12-31 14:29:22 +0900

    12/31 플레이어 대기, 이동 애니매이션 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,29 +7,42 @@ public class Player : MonoBehaviour
 {
     #region Components
     Rigidbody2D _rigid;
+    SpriteRenderer _sprite;
+    Animator _anim;
     #endregion
+
     [SerializeField] Vector2 _inputVec;
     [SerializeField] float _moveSpeed; public float MoveSpeed { get { return _moveSpeed; } }
+
+    public void Init()
+    {
+        if (!TryGetComponent(out _rigid))
+            Debug.LogError("Player _rigid is Null");
+        if (!TryGetComponent(out _sprite))
+            Debug.LogError("Player _sprite is Null");
+        if (!TryGetComponent(out _anim))
+            Debug.LogError("Player _anim is Null");
+        _moveSpeed = 5.0f;
+    }
 
     void Start()
     {
         Init();
     }
-
     void Update()
     {
         //Input();
     }
+
     private void FixedUpdate()
     {
         Move();
     }
 
-    public void Init()
+    private void LateUpdate()
     {
-        if(!TryGetComponent(out _rigid))
-            Debug.LogError("Player _rigid is Null");
-        _moveSpeed = 5.0f;
+        _anim.SetFloat("Speed", _inputVec.magnitude);
+        flip();
     }
 
     void Input()
@@ -37,14 +50,25 @@ public class Player : MonoBehaviour
         float x = UnityEngine.Input.GetAxisRaw("Horizontal");
         float y = UnityEngine.Input.GetAxisRaw("Vertical");
         _inputVec = new Vector2(x, y).normalized;
-    }
+    }                 //구 입력 시스템
 
-    void OnMove(InputValue value)
+    void OnMove(InputValue value)   //신 입력 시스템
     {
         _inputVec = value.Get<Vector2>();
     }
     void Move()
     {
         _rigid.MovePosition(_rigid.position + (_inputVec * _moveSpeed *Time.fixedDeltaTime));
+    }
+
+
+    void flip()
+    {
+        if (_sprite != null) {
+            if (_inputVec.x != 0)
+                _sprite.flipX = _inputVec.x < 0;
+        }
+        else
+            Debug.LogError("Player _sprite is Null");
     }
 }
